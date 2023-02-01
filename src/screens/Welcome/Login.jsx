e717043ec9a307660ae52eee42eb65dd3081fa13
chrisesbueno <alvarezchristopherve@gmail.com>
2023-02-01 19:32:01 -0400

    Eyes Squad

--- src/screens/Welcome/Login.jsx
+++ src/screens/Welcome/Login.jsx
@@ -1,14 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import styles from "@/utils/styles/Welcome.module.css";
 
-const Login = () => {
+const Login = ({navigation, route}) => {
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate(route.params.id)}
+      >
+        <Text>Home</Text>
+      </TouchableOpacity>
     </View>
   )
 }
 
 export default Login
-
-const styles = StyleSheet.create({})
--- src/screens/Welcome/Welcome.jsx
+++ src/screens/Welcome/Welcome.jsx
@@ -2,14 +2,28 @@ import styles from "@/utils/styles/Welcome.module.css";
 import React from "react";
 import { Text, TouchableOpacity, View } from "react-native";
 
-const Welcome = ({ navigation }) => {
+const Welcome = ({ navigation, route }) => {
   return (
     <View style={styles.container}>
       <TouchableOpacity
         style={styles.button}
-        onPress={() => navigation.navigate("Home")}
+        onPress={() => navigation.navigate(route.params.LOGIN, {
+          id: route.params.LOGIN
+        })}
       >
-        <Text>Home</Text>
+        <Text>Login</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate(route.params.REGISTER)}
+      >
+        <Text>Register</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate(route.params.FORGOT)}
+      >
+        <Text>Forgot</Text>
       </TouchableOpacity>
     </View>
   );
