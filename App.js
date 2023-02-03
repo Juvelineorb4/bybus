1159802f86d8cb2f3d739f67ed0e9c71d1293907
Luis Aranguren <ljaranguren@gmail.com>
2023-02-03 09:09:54 -0500

    lo que hizo chris

--- App.js
+++ App.js
@@ -2,10 +2,28 @@ import 'react-native-gesture-handler';
 import Navigation from "@/routes/Navigation";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
-export default function App() {
+// amplify configure
+import { Amplify } from 'aws-amplify'
+// vista login predeterminada
+import { withAuthenticator } from 'aws-amplify-react-native'
+import awsconfig from './src/aws-exports'
+
+
+
+Amplify.configure(awsconfig)
+function App() {
   return (
     <SafeAreaProvider>
       <Navigation />
     </SafeAreaProvider>
   );
 }
+
+export default withAuthenticator(App, {
+  signUpConfig: {
+    hiddenDefaults: ["phone_number"],
+    signUpFields: [
+      { label: "Name", key: "name", required: true, type: "string" }
+    ]
+  }
+});
