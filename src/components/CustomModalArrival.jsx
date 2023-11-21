f8c5ce5c1470eaab6488dd6a94e12cea5ca5e3df
chrisesbueno <alvarezchristopherve@gmail.com>
2023-11-21 16:14:20 -0400

    Cambios

--- src/components/CustomModalArrival.jsx
+++ src/components/CustomModalArrival.jsx
@@ -1,41 +1,53 @@
 import {
-    View,
-    Text,
-    TouchableOpacity,
-    Modal,
-    Pressable,
-    Image,
-    FlatList,
-  } from "react-native";
-  import React, { useEffect, useState } from "react";
-  import { Controller } from "react-hook-form";
-  import styles from "@/utils/styles/CustomModal.module.css";
-  import { useRecoilState } from "recoil";
-  import { routeSearch } from "@/atoms/Modals";
-  import { MaterialCommunityIcons } from '@expo/vector-icons';
-  
-  const CustomModalArrival = ({
-    label,
-    placeholder,
-    name,
-    control,
-    rules,
-    text,
-    data,
-  }) => {
-    const global = require("@/utils/styles/global.js");
-  
-    /* Estaticos */
-    const [selectError, setSelectError] = useState(false);
-    const [modalVisible, setModalVisible] = useState(false);
-    const [push, setPush] = useState(false);
-  
-    /* Guardar valor por item seleccionado */
-    const [selectRoute, setSelectRoute] = useRecoilState(routeSearch);
-  
-  
-    return (
-      <Controller
+  View,
+  Text,
+  TouchableOpacity,
+  Modal,
+  Pressable,
+  Image,
+  FlatList,
+  TextInput
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import { Controller } from "react-hook-form";
+import styles from "@/utils/styles/CustomModal.module.css";
+import { useRecoilState } from "recoil";
+import { routeSearch } from "@/atoms/Modals";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+const CustomModalArrival = ({
+  label,
+  placeholder,
+  name,
+  control,
+  rules,
+  text,
+  data,
+}) => {
+  const global = require("@/utils/styles/global.js");
+
+  /* Estaticos */
+  const [selectError, setSelectError] = useState(false);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [push, setPush] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  /* Guardar valor por item seleccionado */
+  const [selectRoute, setSelectRoute] = useRecoilState(routeSearch);
+  let filteredStates = data.filter((item) =>
+    item.estado.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  // console.log(selectRoute?.arrivalState.estado !== '')
+  let filteredCities =
+    selectRoute?.arrivalState.estado !== ''
+      ? selectRoute?.arrivalState?.ciudades?.filter((item) =>
+          item.toLowerCase().includes(searchTerm.toLowerCase())
+        )
+      : "";
+
+
+  return (
+    <Controller
       control={control}
       name={name}
       rules={rules}
@@ -52,14 +64,18 @@ import {
                 source={require("@/utils/images/target-white.png")}
               /> */}
               <MaterialCommunityIcons name="target" size={24} color="white" />
-                <Text
-                  style={[selectRoute?.arrivalState?.estado ? global.white : global.midGray
-                    ,
-                    styles.inputContainerText
-                  ]}
-                >
-                  {selectRoute?.arrivalState?.estado ? `${selectRoute?.arrivalState?.estado}, ${selectRoute?.arrivalCity}` : placeholder}
-                </Text>
+              <Text
+                style={[
+                  selectRoute?.arrivalState?.estado
+                    ? global.white
+                    : global.midGray,
+                  styles.inputContainerText,
+                ]}
+              >
+                {selectRoute?.arrivalState?.estado
+                  ? `${selectRoute?.arrivalState?.estado}, ${selectRoute?.arrivalCity}`
+                  : placeholder}
+              </Text>
               <Image
                 style={{
                   width: 20,
@@ -84,7 +100,7 @@ import {
                   <Pressable
                     onPress={() => {
                       setModalVisible(!modalVisible);
-                      setPush(false)
+                      setPush(false);
                     }}
                   >
                     <Image
@@ -96,50 +112,106 @@ import {
                       source={require("@/utils/images/arrow_back.png")}
                     />
                   </Pressable>
-                  <Text style={styles.modalText}>{push ? `Seleccione la ciudad` : `Seleccione el estado`}</Text>
+                  <Text style={styles.modalText}>
+                    {push ? `Seleccione la ciudad` : `Seleccione el estado`}
+                  </Text>
                 </View>
-                {!push && <FlatList
-                  data={data}
-                  renderItem={({ item }) => (
-                    <TouchableOpacity
-                      onPress={() => {
-                        setSelectRoute({...selectRoute, arrivalState: item});
-                        setPush(true)
-                      }}
-                      style={[ {
-                        marginVertical: 5,
-
-                      }]}
-                    >
-                      <Text style={[selectRoute?.arrivalState?.estado === item.estado ? global.mainBgColorSecond : global.bgWhite, styles.textListModal]}>{item.estado}</Text>
-                    </TouchableOpacity>
-                  )}
-                  keyExtractor={(item, index) => index}
-                  showsVerticalScrollIndicator={true}
-                />}
-                {selectRoute?.arrivalState && push && <FlatList
-                  data={selectRoute?.arrivalState?.ciudades}
-                  renderItem={({ item }) => (
-                    <TouchableOpacity
-                      onPress={() => {
-                        setSelectRoute({...selectRoute, arrivalCity: item});
-                        setPush(false)
-                        setModalVisible(!modalVisible)
-                      }}
-                      style={[ {
-                        marginVertical: 5,
-
-                      }]}
-                    >
-                      <Text style={[selectRoute?.arrivalCity === item ? global.mainBgColorSecond : global.bgWhite, styles.textListModal]}>{item}</Text>
-                    </TouchableOpacity>
-                  )}
-                  keyExtractor={(item, index) => index}
-                  showsVerticalScrollIndicator={true}
-                />}
+                <View
+                  style={{
+                    borderWidth: 0.5,
+                    borderColor: "black",
+                    borderRadius: 4,
+                    padding: 10,
+                    marginBottom: 25,
+                  }}
+                >
+                  <TextInput
+                    value={searchTerm}
+                    onChangeText={(text) => setSearchTerm(text)}
+                    placeholder="Busca tu estado"
+                    style={{
+                      fontFamily: "regular",
+                    }}
+                  />
+                </View>
+                {!push && (
+                  <FlatList
+                    data={searchTerm !== "" ? filteredStates : data}
+                    renderItem={({ item }) => (
+                      <TouchableOpacity
+                        onPress={() => {
+                          setSelectRoute({
+                            ...selectRoute,
+                            arrivalState: item,
+                          });
+                          setPush(true);
+                          setSearchTerm("");
+                        }}
+                        style={[
+                          {
+                            marginVertical: 5,
+                          },
+                        ]}
+                      >
+                        <Text
+                          style={[
+                            selectRoute?.arrivalState?.estado === item.estado
+                              ? global.mainBgColorSecond
+                              : global.bgWhite,
+                            styles.textListModal,
+                          ]}
+                        >
+                          {item.estado}
+                        </Text>
+                      </TouchableOpacity>
+                    )}
+                    keyExtractor={(item, index) => index}
+                    showsVerticalScrollIndicator={true}
+                  />
+                )}
+                {selectRoute?.arrivalState && push && (
+                  <FlatList
+                    data={
+                      searchTerm
+                        ? filteredCities
+                        : selectRoute?.arrivalState?.ciudades
+                    }
+                    renderItem={({ item }) => (
+                      <TouchableOpacity
+                        onPress={() => {
+                          setSelectRoute({ ...selectRoute, arrivalCity: item });
+                          setPush(false);
+                          setModalVisible(!modalVisible);
+                          setSearchTerm("");
+                        }}
+                        style={[
+                          {
+                            marginVertical: 5,
+                          },
+                        ]}
+                      >
+                        <Text
+                          style={[
+                            selectRoute?.arrivalCity === item
+                              ? global.mainBgColorSecond
+                              : global.bgWhite,
+                            styles.textListModal,
+                          ]}
+                        >
+                          {item}
+                        </Text>
+                      </TouchableOpacity>
+                    )}
+                    keyExtractor={(item, index) => index}
+                    showsVerticalScrollIndicator={true}
+                  />
+                )}
                 <View style={styles.modalBot}>
                   <Text style={styles.modalTextSelect}>
-                    Seleccionaste: {`${selectRoute?.arrivalDeparture?.estado}, ${!push ? selectRoute?.arrivalCity : ''}`}
+                    Seleccionaste:{" "}
+                    {`${selectRoute?.arrivalDeparture?.estado}, ${
+                      !push ? selectRoute?.arrivalCity : ""
+                    }`}
                   </Text>
                 </View>
               </View>
@@ -148,8 +220,7 @@ import {
         </>
       )}
     />
-    );
-  };
-  
-  export default CustomModalArrival;
-  
+  );
+};
+
+export default CustomModalArrival;
--- src/components/CustomModalDeparture.jsx
+++ src/components/CustomModalDeparture.jsx
@@ -6,13 +6,14 @@ import {
   Pressable,
   Image,
   FlatList,
+  TextInput,
 } from "react-native";
 import React, { useEffect, useState } from "react";
 import { Controller } from "react-hook-form";
 import styles from "@/utils/styles/CustomModal.module.css";
 import { useRecoilState } from "recoil";
 import { routeSearch } from "@/atoms/Modals";
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const CustomModalDeparture = ({
   label,
@@ -28,11 +29,19 @@ const CustomModalDeparture = ({
   /* Estaticos */
   const [selectError, setSelectError] = useState(false);
   const [modalVisible, setModalVisible] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
   const [push, setPush] = useState(false);
-
   /* Guardar valor por item seleccionado */
   const [selectRoute, setSelectRoute] = useRecoilState(routeSearch);
-
+  let filteredStates = data.filter((item) =>
+    item.estado.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  let filteredCities =
+    selectRoute?.departureState.estado !== ""
+      ? selectRoute?.departureState?.ciudades?.filter((item) =>
+          item.toLowerCase().includes(searchTerm.toLowerCase())
+        )
+      : "";
 
   return (
     <Controller
@@ -52,14 +61,18 @@ const CustomModalDeparture = ({
                 source={require("@/utils/images/target-white.png")}
               /> */}
               <MaterialCommunityIcons name="target" size={24} color="white" />
-                <Text
-                  style={[selectRoute?.departureState?.estado ? global.white : global.midGray
-                    ,
-                    styles.inputContainerText
-                  ]}
-                >
-                  {selectRoute?.departureState?.estado ? `${selectRoute?.departureState?.estado}, ${selectRoute?.departureCity}` : placeholder}
-                </Text>
+              <Text
+                style={[
+                  selectRoute?.departureState?.estado
+                    ? global.white
+                    : global.midGray,
+                  styles.inputContainerText,
+                ]}
+              >
+                {selectRoute?.departureState?.estado
+                  ? `${selectRoute?.departureState?.estado}, ${selectRoute?.departureCity}`
+                  : placeholder}
+              </Text>
               <Image
                 style={{
                   width: 20,
@@ -84,7 +97,7 @@ const CustomModalDeparture = ({
                   <Pressable
                     onPress={() => {
                       setModalVisible(!modalVisible);
-                      setPush(false)
+                      setPush(false);
                     }}
                   >
                     <Image
@@ -96,50 +109,109 @@ const CustomModalDeparture = ({
                       source={require("@/utils/images/arrow_back.png")}
                     />
                   </Pressable>
-                  <Text style={styles.modalText}>{push ? `Seleccione la ciudad` : `Seleccione el estado`}</Text>
+                  <Text style={styles.modalText}>
+                    {push ? `Seleccione la ciudad` : `Seleccione el estado`}
+                  </Text>
                 </View>
-                {!push && <FlatList
-                  data={data}
-                  renderItem={({ item }) => (
-                    <TouchableOpacity
-                      onPress={() => {
-                        setSelectRoute({...selectRoute, departureState: item});
-                        setPush(true)
-                      }}
-                      style={[ {
-                        marginVertical: 5,
-
-                      }]}
-                    >
-                      <Text style={[selectRoute?.departureState?.estado === item.estado ? global.mainBgColorSecond : global.bgWhite, styles.textListModal]}>{item.estado}</Text>
-                    </TouchableOpacity>
-                  )}
-                  keyExtractor={(item, index) => index}
-                  showsVerticalScrollIndicator={true}
-                />}
-                {selectRoute?.departureState && push && <FlatList
-                  data={selectRoute?.departureState?.ciudades}
-                  renderItem={({ item }) => (
-                    <TouchableOpacity
-                      onPress={() => {
-                        setSelectRoute({...selectRoute, departureCity: item});
-                        setPush(false)
-                        setModalVisible(!modalVisible)
-                      }}
-                      style={[ {
-                        marginVertical: 5,
-
-                      }]}
-                    >
-                      <Text style={[selectRoute?.departureCity === item ? global.mainBgColorSecond : global.bgWhite, styles.textListModal]}>{item}</Text>
-                    </TouchableOpacity>
-                  )}
-                  keyExtractor={(item, index) => index}
-                  showsVerticalScrollIndicator={true}
-                />}
+                <View
+                  style={{
+                    borderWidth: 0.5,
+                    borderColor: "black",
+                    borderRadius: 4,
+                    padding: 10,
+                    marginBottom: 25,
+                  }}
+                >
+                  <TextInput
+                    value={searchTerm}
+                    onChangeText={(text) => setSearchTerm(text)}
+                    placeholder="Busca tu estado"
+                    style={{
+                      fontFamily: "regular",
+                    }}
+                  />
+                </View>
+                {!push && (
+                  <FlatList
+                    data={searchTerm !== "" ? filteredStates : data}
+                    renderItem={({ item }) => (
+                      <TouchableOpacity
+                        onPress={() => {
+                          setSelectRoute({
+                            ...selectRoute,
+                            departureState: item,
+                          });
+                          setPush(true);
+                          setSearchTerm("");
+                        }}
+                        style={[
+                          {
+                            marginVertical: 5,
+                          },
+                        ]}
+                      >
+                        <Text
+                          style={[
+                            selectRoute?.departureState?.estado === item.estado
+                              ? global.mainBgColorSecond
+                              : global.bgWhite,
+                            styles.textListModal,
+                          ]}
+                        >
+                          {item.estado}
+                        </Text>
+                      </TouchableOpacity>
+                    )}
+                    keyExtractor={(item, index) => index}
+                    showsVerticalScrollIndicator={true}
+                  />
+                )}
+                {selectRoute?.departureState && push && (
+                  <FlatList
+                    data={
+                      searchTerm
+                        ? filteredCities
+                        : selectRoute?.departureState?.ciudades
+                    }
+                    renderItem={({ item }) => (
+                      <TouchableOpacity
+                        onPress={() => {
+                          setSelectRoute({
+                            ...selectRoute,
+                            departureCity: item,
+                          });
+                          setPush(false);
+                          setModalVisible(!modalVisible);
+                          setSearchTerm("");
+                        }}
+                        style={[
+                          {
+                            marginVertical: 5,
+                          },
+                        ]}
+                      >
+                        <Text
+                          style={[
+                            selectRoute?.departureCity === item
+                              ? global.mainBgColorSecond
+                              : global.bgWhite,
+                            styles.textListModal,
+                          ]}
+                        >
+                          {item}
+                        </Text>
+                      </TouchableOpacity>
+                    )}
+                    keyExtractor={(item, index) => index}
+                    showsVerticalScrollIndicator={true}
+                  />
+                )}
                 <View style={styles.modalBot}>
                   <Text style={styles.modalTextSelect}>
-                    Seleccionaste: {`${selectRoute?.departureState?.estado}, ${!push ? selectRoute.departureCity : ''}`}
+                    Seleccionaste:{" "}
+                    {`${selectRoute?.departureState?.estado}, ${
+                      !push ? selectRoute.departureCity : ""
+                    }`}
                   </Text>
                 </View>
               </View>
