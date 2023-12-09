d814ccea9f83888e4fca24f6ebbbf33fcd3b2ad8
chrisesbueno <alvarezchristopherve@gmail.com>
2023-12-09 08:35:46 -0400

    Cambios

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -70,15 +70,7 @@ export type AmplifyDependentResourcesAttributes = {
       "Name": "string",
       "Region": "string"
     },
-    "reprogramBooking": {
-      "Arn": "string",
-      "CloudWatchEventRule": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    },
-    "reprogramBookingTicket": {
+    "scheduleBookingAgency": {
       "Arn": "string",
       "LambdaExecutionRole": "string",
       "LambdaExecutionRoleArn": "string",
