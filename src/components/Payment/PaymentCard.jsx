beaaf7c6ff68e960c90286302d6e7564039a0e27
chrisesbueno <alvarezchristopherve@gmail.com>
2023-12-27 15:41:43 -0400

    Cambios

--- src/components/Payment/PaymentCard.jsx
+++ src/components/Payment/PaymentCard.jsx
@@ -1,85 +1,82 @@
-import { Image, ScrollView, Text, View, Alert } from "react-native";
+import { Image, ScrollView, Text, View, Alert, TextInput } from "react-native";
 import React, { useState } from "react";
 import CustomInput from "../CustomInput";
 import { useForm } from "react-hook-form";
 import CustomButton from "../CustomButton";
 import styles from "../../utils/styles/Payment.module.css";
-import { AntDesign } from '@expo/vector-icons';
+import { AntDesign } from "@expo/vector-icons";
 
 const PaymentCard = ({ button, text, order, handlePress }) => {
   const global = require("@/utils/styles/global.js");
-  const { control, handleSubmit } = useForm();
-  const [aproved, setAproved] = useState(false)
+  const [aproved, setAproved] = useState(false);
+  const [error, setError] = useState(false);
+  const [reference, setReference] = useState('');
   return (
     <View style={styles.container}>
-      <View style={styles.images}>
-        <Image
-          style={{
-            width: 50,
-            height: 20,
-            resizeMode: "contain",
-          }}
-          source={require("@/utils/images/mastercard.png")}
-        />
-        <Image
-          style={{
-            width: 50,
-            height: 20,
-            resizeMode: "contain",
-          }}
-          source={require("@/utils/images/visa.png")}
-        />
-      </View>
       <View style={styles.layout}>
-        <CustomInput
-          control={control}
-          name={`cardNumber`}
-          placeholder={"0000 0000 0000 0000"}
-          styled={{
-            text: styles.textCardNumber,
-            error: styles.errorCardNumber,
-            input: [styles.inputCardNumber, global.bgWhiteSoft],
+        <Text style={{
+          fontFamily: 'medium',
+          fontSize: 16
+        }}>Monto total en Bs: 1.00</Text>
+        <TextInput
+          value={reference}
+          onChangeText={(e) => setReference(e)}
+          placeholder={`Numero de referencia del pago movil`}
+          style={reference ? {
+            flex: 1,
+            borderColor: '#333',
+            padding: 10,
+            fontFamily: 'regular',
+            borderWidth: 0.5,
+            borderRadius: 8,
+            marginVertical: 10
+
+          } : {
+            flex: 1,
+            borderColor: '#333',
+            padding: 10,
+            fontFamily: 'regular',
+            borderWidth: 0.5,
+            borderRadius: 8,
+            borderWidth: 0.5,
+            marginVertical: 10
           }}
         />
-        <View style={styles.inputs}>
-          <CustomInput
-            control={control}
-            name={`cardExpiration`}
-            placeholder={"MM/YY"}
-            styled={{
-              text: [styles.textCardDate, global.black],
-              error: styles.errorCardDate,
-              input: [styles.inputCardDate, global.bgWhiteSoft],
-            }}
-          />
-          <CustomInput
-            control={control}
-            name={`cardSecuredCode`}
-            placeholder={"XXXX"}
-            styled={{
-              text: [styles.textCardCode, global.black],
-              error: styles.errorCardCode,
-              input: [styles.inputCardCode, global.bgWhiteSoft],
-            }}
-          />
-        </View>
+        {error ? <Text style={{fontFamily: 'regular', color: 'red', fontSize: 12}}>Tienes que colocar un numero de referencia para poder darle a pagar</Text> : ''}
         {button && (
           <View style={styles.button}>
-            {aproved && <View style={{marginRight: 10, flexDirection: 'row', alignItems: 'center'}}>
-            <AntDesign name="checkcircleo" size={16} color="green" />
-            <Text style={{fontSize: 14, fontFamily: 'thin', marginLeft: 4}}>Pago aprobado</Text>
-            </View>}
+            {aproved && (
+              <View
+                style={{
+                  marginRight: 10,
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <AntDesign name="checkcircleo" size={16} color="green" />
+                <Text
+                  style={{ fontSize: 14, fontFamily: "thin", marginLeft: 4 }}
+                >
+                  Pago por confirmar
+                </Text>
+              </View>
+            )}
             <CustomButton
               text={text}
               handlePress={() => {
                 if (aproved) return;
+                if (reference === '') {
+                  setError(true)
+                  return
+                }
                 Alert.alert(
                   "Orden de pago",
-                  `Has pagado $${order.mount}.00 por ${order.quantity} boleto(s), para ${order.arrival}, a nombre(s) de ${order.id}`,
+                  `Has pagado $${order.mount}.00 por ${order.quantity} boleto(s), para ${order.arrival}, a nombre(s) de ${reference}`,
                   [{ text: "Aceptar", onPress: () => console.log("Aceptado") }]
                 );
-                setAproved(true)
-                handlePress()
+                setError(false)
+                setAproved(true);
+                handlePress(reference);
               }}
               textStyles={[styles.btnText, global.white]}
               buttonStyles={[styles.btnBg, global.mainBgColor]}
