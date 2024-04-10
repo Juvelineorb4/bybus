d6e0d64fc851a35f6a1028f96b18d4854e5829c1
Juvelineorb4 <73950715+Juvelineorb4@users.noreply.github.com>
2024-04-10 10:45:52 -0400

    cambios

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -63,13 +63,6 @@ export type AmplifyDependentResourcesAttributes = {
       "Name": "string",
       "Region": "string"
     },
-    "getTodayTasaCambio": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    },
     "preSignInCognito": {
       "Arn": "string",
       "LambdaExecutionRole": "string",
@@ -87,21 +80,6 @@ export type AmplifyDependentResourcesAttributes = {
     "sheduleBookingUpdate": {
       "Arn": "string",
       "CloudWatchEventRule": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    },
-    "tasaCambio": {
-      "Arn": "string",
-      "CloudWatchEventRule": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    },
-    "updateAgencyCognito": {
-      "Arn": "string",
       "LambdaExecutionRole": "string",
       "LambdaExecutionRoleArn": "string",
       "Name": "string",
