18a8da6be93b6139761a1609ecfee1df17d0c0be
Juvelineorb4 <73950715+Juvelineorb4@users.noreply.github.com>
2024-04-15 16:21:30 -0400

    cambios

--- amplify/backend/function/uploadAgencyImage/src/index.js
+++ amplify/backend/function/uploadAgencyImage/src/index.js
@@ -5,6 +5,7 @@
 	REGION
 	STORAGE_S3STORAGEBYBUS_BUCKETNAME
 Amplify Params - DO NOT EDIT */
+
 import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import crypto from "@aws-crypto/sha256-js";
 import { defaultProvider } from "@aws-sdk/credential-provider-node";
